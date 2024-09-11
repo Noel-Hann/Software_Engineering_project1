44f3f0cde97f566b00d25b9db761b7b1b444d1d3
vincentmpalma <vincentmichaelpalma@gmail.com>
2024-09-11 15:36:12 -0700

    Added database to let users sign up and sign in. Code needs refactoring, still a work in progress. But it is working (for the most part). Some bugs still have to be ironed out.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,34 @@
 import { Stack } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerTitle: "Log In",
-          headerStyle: { backgroundColor: "black" },
-          headerTintColor: "white",
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="home/[user_id]"
-        options={{
-          headerTitle: "Home Page",
-        }}
-      />
-      <Stack.Screen
-        name="spotify/[user_id]"
-        options={{
-          headerTitle: "Spotify API Page",
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <SQLiteProvider databaseName="example.db">
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerTitle: "Log In",
+            headerStyle: { backgroundColor: "black" },
+            headerTintColor: "white",
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="home/[user_id]"
+          options={{
+            headerTitle: "Home Page",
+          }}
+        />
+        <Stack.Screen
+          name="spotify/[user_id]"
+          options={{
+            headerTitle: "Spotify API Page",
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </SQLiteProvider>
   );
 };
 
--- app/(tabs)/home/[user_id].tsx
+++ app/(tabs)/home/[user_id].tsx
@@ -1,7 +1,14 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link, useLocalSearchParams } from "expo-router";
+import { useSQLiteContext } from "expo-sqlite";
+
+interface User {
+  id: number;
+  username: string;
+  password: string;
+}
 
 const Home = () => {
   const { user_id } = useLocalSearchParams();
@@ -14,6 +21,29 @@ const Home = () => {
       <Link href="/">Go Back</Link>
     </SafeAreaView>
   );
+};
+
+const db = async () => {
+  console.log("is db in home screen");
+  const db = useSQLiteContext(); //getting context of sql db
+  const [users, setUsers] = useState<User[]>([]); //creating a user state of all users
+
+  console.log("Getting users... in home screen");
+  //function to get all users
+  const getUsers = async () => {
+    try {
+      const userRows = (await db.getAllAsync("SELECT * FROM users")) as User[];
+      setUsers(userRows);
+      console.log("Users row set: ", userRows);
+    } catch (error) {
+      console.log("Error while loading users : ", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("In useEffect for SIGN IN users db...");
+    getUsers();
+  }, []);
 };
 
 const styles = StyleSheet.create({
